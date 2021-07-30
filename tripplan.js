31a5f5a3b8a97a33c28495421246c6f5510b085a
Treveon Edwards <fasuke322@gmail.com>
2021-07-30 14:52:44 -0400

    add bolier plater to html. copy/pasted js from vwsc to repo

---
+++ tripplan.js
@@ -0,0 +1,11 @@
+"use strict";
+let location = [sapporo,tokyo,kyoto,ikkebukuro]
+let userInputLocay = prompt("where would you like to vacation?")
+function selectTravelDest(pickMePlace,rndomPlace){
+for(i=0; location.length; i++)
+if if userInputLocay   
+console.log(location.random)
+}
+
+
+selectTravelDest(location,userInputLocay)
