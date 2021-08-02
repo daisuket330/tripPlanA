be9237cf11e53e2097b7f1a5d498ebd947847695
Treveon Edwards <fasuke322@gmail.com>
2021-08-02 12:55:54 -0400

    attempted to create function that generates arandom location based on user input

--- tripplan.js
+++ tripplan.js
@@ -1,11 +1,45 @@
-"use strict";
-let location = [sapporo,tokyo,kyoto,ikkebukuro]
+
+
+let locationz = ["sapporo","tokyo","kyoto","ikkebukuro"]
 let userInputLocay = prompt("where would you like to vacation?")
-function selectTravelDest(pickMePlace,rndomPlace){
-for(i=0; location.length; i++)
-if if userInputLocay   
-console.log(location.random)
+function selectTravelDest(array,userInput){
+
+    for(i=0; i<array.length; i++){
+
+        if (userInput === "yes"){
+
+            console.log("i'll be happy to get you to " + array[i]) 
+        }
+        else{
+            alert("this location is not available")
+        
+    
+let result = getRandomPlace[array];
+let inputVacay = prompt("would you like to go to" + result)
+function getRandomPlace(randoarry,touristInput) {
+
+    for (i=0; i<array.length; i++){
+        if (touristInput === "Yes"){
+            alert("enjoy your trip to" + randoarry, "!")
+        
+        break }
+    }
+    let randomIndex = Math.floor(Math.random() * array.length);
+
+    
+    let item = array[randomIndex];
+
+    return item;
 }
 
 
-selectTravelDest(location,userInputLocay)
+
+   
+    
+}
+ 
+
+}
+
+getRandomPlace(result,inputVacay)
+selectTravelDest(locationz,userInputLocay)}
